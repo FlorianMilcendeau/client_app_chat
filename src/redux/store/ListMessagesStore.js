1512dea415f3d6be9f8b672605376d187c0ff28c
Florian Milcendeau <fmilcendeau85@gmail.com>
2021-02-03 17:04:35 +0100

    Addition: state user id

--- src/redux/store/ListMessagesStore.js
+++ src/redux/store/ListMessagesStore.js
@@ -1,9 +1,13 @@
 import { connect } from 'react-redux';
+
 import ListMessage from '../../components/common/ListMessage/ListMessage';
+
 import { messagesSelector } from '../selectors/channelSelector';
+import { userIdSelector } from '../selectors/userSelector';
 
 const mapStateToProps = (state) => ({
   messages: messagesSelector(state),
+  userId: userIdSelector(state),
 });
 
 export const ListMessagesStore = connect(mapStateToProps, null)(ListMessage);
