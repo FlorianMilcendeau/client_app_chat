a1a1cb2482a1b2fde06bf767df83e3c12d7ef0b8
Florian Milcendeau <fmilcendeau85@gmail.com>
2021-02-04 12:19:48 +0100

    Addition: condition

--- src/components/common/Label/Label.jsx
+++ src/components/common/Label/Label.jsx
@@ -5,8 +5,11 @@ import styles from './Label.module.css';
 
 const Label = ({ name }) => {
   // I get the first two initials.
-  const split = name.split(' ');
-  const initial = split.map((el) => el[0].toUpperCase()).slice(0, 2);
+  let initial = '';
+  if (name) {
+    const split = name.split(' ');
+    initial = split.map((el) => el[0].toUpperCase()).slice(0, 2);
+  }
 
   // Random color.
   const palette = ['#ffb3ba', '#ffdfba', '#bae1ff'];
